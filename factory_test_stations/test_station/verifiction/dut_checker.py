976baac57b438e075efe78806361db3aa8a06d8b
T.C.H <ttcchh2008@qq.com>
2019-11-08 19:37:46 +0800

    * optimize dut checker.

--- factory_test_stations/test_station/verifiction/dut_checker.py
+++ factory_test_stations/test_station/verifiction/dut_checker.py
@@ -17,19 +17,21 @@ class DutChecker(object):
         self._template_dir = None
         self._minArea = station_config.DISP_CHECKER_MIN
         self._station_config = station_config
+        self._dst = None
 
     def _blob_areas(self, image1):
         hsv = cv.cvtColor(image1, cv.COLOR_BGR2HSV)
-        lower = np.array([127])
-        upper = np.array([255])
-        img = cv.inRange(hsv[:, :, 2], lower, upper)
-        kernel = np.ones((11, 11), dtype=np.uint8)
+        lower = np.array([self._station_config.DISP_CHECKER_THRESH_LOW])
+        upper = np.array([self._station_config.DISP_CHECKER_THRESH_HIGH])
+        h, s, v = cv.split(hsv)
+        img = cv.inRange(v, lower, upper)
+
+        kernel = np.ones((5, 5), dtype=np.uint8)
         img = cv.erode(img, kernel, iterations=1)
         dst = cv.dilate(img, kernel, iterations=1)
 
-        img, contours, hierarchy = cv.findContours(dst.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        cv.drawContours(dst, contours, -1, (0, 0, 255), 3)
-
+        img, contours, hierarchy = cv.findContours(dst.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        self._dst = cv.drawContours(dst, contours, -1, (0, 0, 255), 1, lineType=cv.LINE_8)
         areas = []
         for c in contours:
             area = cv.contourArea(c)
@@ -67,3 +69,36 @@ class DutChecker(object):
         if self._latest_image is not None:
             fn = os.path.join(self._template_dir, name)
             cv.imwrite(fn, self._latest_image)
+        if self._dst is not  None:
+            fn = os.path.join(self._template_dir, 'dst_'+name)
+            cv.imwrite(fn, self._dst)
+
+def print_to_console(self, msg):
+    pass
+
+if __name__ == "__main__" :
+    import sys
+    import types
+    sys.path.append(r'..\..')
+    import station_config
+    import logging
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logging.getLogger(__name__).addHandler(ch)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+
+    station_config.load_station('pancake_pixel')
+    station_config.DISP_CHECKER_CAMERA_INDEX = 1
+    # station_config.load_station('pancake_uniformity')
+    station_config.print_to_console = types.MethodType(print_to_console, station_config)
+
+    the_unit = DutChecker(station_config)
+    the_unit.initialize()
+
+    for c in range(1, 100):
+        print the_unit.do_checker()
+        the_unit.save_log_img("x.bmp")
+        pass
+
+    the_unit.close()
