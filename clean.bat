c427bf347ca6eaf06398c99433f09dc014d25bae
flying-pisces <yyin@me.com>
2020-04-24 11:46:28 -0700

    add clean.bat

--- clean.bat
+++ clean.bat
@@ -1,3 +1,5 @@
 del /s *.pyc
 del /s *.*~
 FOR /d /r . %%d IN ("__pycache__") DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN ("CaptureFolder1") DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN ("CaptureFolder2") DO @IF EXIST "%%d" rd /s /q "%%d"
